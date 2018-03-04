1ae1b005fbe43db9c629fb56f6732df3a6e71ffb
Ema-jar <emanuele.giarlini@gmail.com>
2018-03-04 23:21:02 +0100

    add base to the tree

--- pythonChristmasTree/christmasTree.py
+++ pythonChristmasTree/christmasTree.py
@@ -2,16 +2,17 @@
 
 treeHeight = input("Quanto deve essere alto l'albero?")
 
-if treeHeight < 1:
-    print "L'albero deve essere alto almeno 1"
+if treeHeight < 2:
+    print "L'albero deve essere alto almeno 2"
     exit()
 
 currentWidth = 0
-maxWidth = (treeHeight * 2) + 1
+maxTreeWidth = (treeHeight * 2) + 1
 whiteSpaces = treeHeight
 
+# print tree
 for i in range(0, treeHeight):
-    stars = maxWidth - (whiteSpaces * 2)
+    stars = maxTreeWidth - (whiteSpaces * 2)
 
     for j in range (0, whiteSpaces):
         print " ",
@@ -23,6 +24,22 @@ for i in range(0, treeHeight):
     print "\n"
     whiteSpaces -= 1
 
+# print base
+baseWidth = treeHeight / 3
+if baseWidth != 1:
+    baseWidth += 1
 
+baseHeight = 2
+whiteSpaces = treeHeight
 
+for i in range(0, baseHeight):
+
+    for j in range(0, (maxTreeWidth - baseWidth) / 2):
+        print " ",
+    for j in range(0, baseWidth):
+        print "*",
+    for j in range(0, (maxTreeWidth - baseWidth) / 2):
+        print " ",
+
+    print "\n"
 
