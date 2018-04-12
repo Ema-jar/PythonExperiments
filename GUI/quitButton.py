8edef870e221d49e16295fb6559f3e8d43c59717
Ema-jar <emanuele.giarlini@gmail.com>
2018-04-12 23:14:24 +0200

    exercise for lesson

--- GUI/quitButton.py
+++ GUI/quitButton.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python      
 import Tkinter as tk       
 
+
 class Application(tk.Frame):              
     def __init__(self, master=None):
-        tk.Frame.__init__(self, master)   
+        tk.Frame.__init__(self, master)
         self.grid()                       
-        self.createWidgets()
+        self.create_widgets()
 
-    def createWidgets(self):
-        self.quitButton = tk.Button(self, text='Quit',
-            command=self.quit)            
+    def create_widgets(self):
+        self.quitButton = tk.Button(self, text='Quit', command=self.quit)
         self.quitButton.grid()            
 
 app = Application()                       
